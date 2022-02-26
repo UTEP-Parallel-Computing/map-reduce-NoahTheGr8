976c8c60a5157626bb5e7e36d8946905c41fc721
Noah <rnpadilla2@miners.utep.edu>
2022-02-25 19:36:08 -0700

    complete code and report

--- MapReduce.py
+++ MapReduce.py
@@ -22,9 +22,9 @@ counts how many times kw is in all_words
 
 '''
 def count(kw, all_words):
-    count = all_words.count(kw)
+    count = all_words.count(kw.lower())
     print("Found ", count, "for ", kw)
-    return count   
+    return count
 
 
 if __name__ == "__main__":
@@ -45,11 +45,11 @@ if __name__ == "__main__":
     print("Filtering the Data...")
     #get all words from all documents
     words = []
-    for doc in readings[:1]:
-        words.append(re.split('\n| ',doc))    
+    for doc in readings[:]:
+        words.append(re.split('\n| |,',doc))    
 
 
-    #below populates list with ALL words from ALL documents without any spaces ||| words_new is a 1D list of strings
+    #below populates 'words_new' list with ALL words from ALL documents without any spaces ||| words_new is a 1D list of strings
     words_new = [] 
     for i in range(len(words)):
         for word in words[i]:
@@ -57,39 +57,61 @@ if __name__ == "__main__":
                 words_new.append(word)
     print("Total amount of words ",len(words_new))
     print("Finished Filtering the Data...")
-    #--------------------------------------------- MAP ---------------------------------------   
+    #--------------------------------------------- MAP->SHUFFLE->REDUCE ---------------------------------------   
     #print(len(words_new)) #198,323 words
     # start a parallel section
     print("Mapping the data...")
     start = time.time()
-     
-        
-    with pymp.Parallel(4) as p:
-        
-
+    
+    global keywords
+    global kw_dict
+    global kw_dictShared
+    with pymp.Parallel() as p:        
         # create a shared dict since bug in OpenMP with outside initialization
         #the shared list of all keywords we need to find
-        keywords = pymp.shared.list(["hate", "love", "death", "night", "sleep", "time", "henry", "hamlet", "you",
-                                     "my", "blood", "poison", "macbeth", "king", "heart", "honest"])        
+        keywords = pymp.shared.list(["hate", "love", "death", "night", "sleep", "time", "henry", "hamlet", "you", "my", "blood", "poison", "macbeth", "king", "heart", "honest"])        
         kw_dict = dict.fromkeys(keywords, 0)         
         kw_dictShared = pymp.shared.dict(kw_dict)
         
-        #print("In thread:", p.thread_num)      
-        countLock = p.lock
-        #print("TN>>>>>>>>>>>>>>>>>>>>>",p.thread_num)
         for key_word in p.iterate(keywords):
-            print("Thread ", p.thread_num , " : ", key_word)    
-            countLock.acquire()
             #Below is mapping words and their frequency
-            kw_dictShared[key_word] += count(key_word, words_new)
-            countLock.release()
+            kw_dictShared[key_word] = count(key_word, words_new)
       
-        #print( "Thread", p.thread_num, "\n", kw_dictShared)
-    print(kw_dictShared)
+        print(kw_dictShared)
     elapsed_time = time.time() - start
-    print("Elapsed time in seconds for multithreading: ",elapsed_time)
-    #------------------------Shuffle and Reduce Method ------------------------------
-    
+    print("Elapsed time in seconds for calculating all the word frequencies: ",elapsed_time)
+
+'''
+----------------------------------------- EXAMPLE OUTPUT 1 THREAD -----------------------------------------
+
+Filtering the Data...
+Total amount of words  961909
+Finished Filtering the Data...
+Mapping the data...
+Found  150 for  hate
+Found  1746 for  love
+Found  563 for  death
+Found  493 for  night
+Found  175 for  sleep
+Found  899 for  time
+Found  0 for  henry
+Found  0 for  hamlet
+Found  11172 for  you
+Found  11425 for  my
+Found  530 for  blood
+Found  65 for  poison
+Found  0 for  macbeth
+Found  296 for  king
+Found  775 for  heart
+Found  246 for  honest
+{'hate': 150, 'love': 1746, 'death': 563, 'night': 493, 'sleep': 175, 'time': 899, 'henry': 0, 'hamlet': 0, 'you': 11172, 'my': 11425, 'blood': 530, 'poison': 65, 'macbeth': 0, 'king': 296, 'heart': 775, 'honest': 246}
+Elapsed time in seconds for calculating all the word frequencies:  0.1938166618347168
+
+
+
+
+
+'''   
     
     
     
--- Report_MapReduce.md
+++ Report_MapReduce.md
@@ -3,34 +3,58 @@
 This programs main purpose is to perform the map reduce algorithm done in parallel in python.
 
 ## Problems Encountered and how I overcame them
-
+The main issue that I was having was that OpenMP was having the shared dictionary sync with all the threads but could not get it going. I think this is because the OpenMP library is buggy thus leaving issues to the programmer like freezing or locking if the shared dictionary is instantiated outside of the parallel section. 
     
-## Total Time it Took to Complete
-Total time it took was ~ hours because of the shared memory issues mentioned aboves.
+## Total Time Taken
+Total time it took was ~10 hours because of the shared memory issue..
 
 ## Performance Measurements
-The performance of the program was measured by taking the performance time 'time.perf_time()'. The results are shown below in seconds. 
+The performance of the program was measured by taking the performance time 'time.time()'. The results are shown below in seconds. 
 
 
-* 1 thread: 
+ * 1 thread: 0.1938166618347168
 
 
-
-* 2 threads: 
-
+ * 2 threads: 0.24344372749328613
 
 
-* 4 threads: 
+ * 4 threads: 0.2616910934448242
 
 
-* 8 threads:
-
+ * 8 threads: 2.1395139694213867
 
 
 ## Program Behavior
+It seems that the performance peaked at about 2 threads and then declined. An output run for 1 thread looks like the below -
+
+```
+Filtering the Data...
+Total amount of words  961909
+Finished Filtering the Data...
+Mapping the data...
+Found  150 for  hate
+Found  1746 for  love
+Found  563 for  death
+Found  493 for  night
+Found  175 for  sleep
+Found  899 for  time
+Found  0 for  henry
+Found  0 for  hamlet
+Found  11172 for  you
+Found  11425 for  my
+Found  530 for  blood
+Found  65 for  poison
+Found  0 for  macbeth
+Found  296 for  king
+Found  775 for  heart
+Found  246 for  honest
+{'hate': 150, 'love': 1746, 'death': 563, 'night': 493, 'sleep': 175, 'time': 899, 'henry': 0, 'hamlet': 0, 'you': 11172, 'my': 11425, 'blood': 530, 'poison': 65, 'macbeth': 0, 'king': 296, 'heart': 775, 'honest': 246}
+Elapsed time in seconds for calculating all the word frequencies:  0.19500064849853516
+```
 
 ## Observations/Comments
-.
+I think that this assignment was good since I leared how to use a shared list and dictionary within the OpenMP libray for the most part. I think that the OpenMP bug really needs to be fixed asap! Other than that, it was a nice lab.
+
 
 ## CPU Info Dump
 * model name	: 11th Gen Intel(R) Core(TM) i5-11400F @ 2.60GHz
